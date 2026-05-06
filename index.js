671bd1fbbe3efc9be198980d903134fbec64fb55
No_Name <105019557+ah2355@users.noreply.github.com>
2026-05-06 11:06:44 -0400

    Making the landing page

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
+const path = require("path");
 const port = 3001;
 const app = express();
+
+app.use(express.static(__dirname));
+
 app.get('/', (req,res) =>{
     res.send(`<!DOCTYPE html>
         <html>
             <head>
                 <meta charset = "utf-8">
-                <link rel = "stylsheet" href= "/style.css">
+                <link rel = "stylesheet" href= "style.css">
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
                 <title>Final Project</title>
+                <script src = "func.js"></script>
             </head>
             <body>
-                <h1>Final Project :3</h1>
+                <div id = introduction>
+                    <h1>Final Project :3</h1>
+                    <p>Going to write smth</p>
+                </div>
             </body>
         </html>
         `)
