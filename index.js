97e54c8e6d7dacc6e1a6420eeaccfb5bac557a5f
No_Name <105019557+ah2355@users.noreply.github.com>
2026-05-06 10:35:20 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const port = 3001;
+const app = express();
+app.get('/', (req,res) =>{
+    res.send(`<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset = "utf-8">
+                <link rel = "stylsheet" href= "/style.css">
+                <title>Final Project</title>
+            </head>
+            <body>
+                <h1>Final Project :3</h1>
+            </body>
+        </html>
+        `)
+})
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log("Server failed: " + err);
+    } else {
+        console.log(`http://localhost:${port}`);
+    }
+});
