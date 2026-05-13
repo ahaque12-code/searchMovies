576c29891ba917cf710730f60ffb4012c3c32b99
No_Name <105019557+ah2355@users.noreply.github.com>
2026-05-13 12:33:46 -0400

    Made the API to work

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
-const port = 3001;
+require('dotenv').config();
+const port = process.env.PORT || 3001;
 const app = express();
+ 
 
 app.use(express.static(__dirname));
 
@@ -25,8 +27,10 @@ app.get('/', (req,res) => {
 
                 <div id = "movieBody">
                     <h2>Making searching movies easier</h2> 
+                    <h3>You can either search for a specific movie</h3> 
+                    <h3>or just put ratings, genre, year to browse collections</h3>
                     <form id = "movieForm" action = "/results" method = "get">
-                     <input type = "text" id = "movieName" placeholder = "Search movies, shows..." >
+                     <input type = "text" name = "q" id = "movieName" placeholder = "Search movies, shows..." ><br><br>
                      <input type = "number" id = "movieRating" max = 10 min = 0 placeholder = "Ratings (0-10)" >
                      <input type = "button" id = "movieGenre" value = "Genre">
                      <input type = "text" id = "yearRelease" placeholder = "Year"><br><br>
@@ -39,25 +43,44 @@ app.get('/', (req,res) => {
     }
 )
 
-app.get("/results",(req,res) => {
-    res.send(`<!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset = "utf-8">
-                <link rel = "stylesheet" href= "style.css">
-                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
-                <title>Info</title>
-                <script src="func.js" defer></script>
-            </head>
-            <body>
-                <div id = info>
-                    <h1>Search Result</h1>
-                    <p>Going to write smth</p>
-                </div>
-            </body>
-        </html>
-        `)
-    } )
+app.get("/results", async (req,res) => {
+    const searchMovie = req.query.q;
+    const api_key = process.env.RAPID_API_KEY;
+    const api_host = process.env.RAPID_API_HOST;
+
+    if (!searchMovie) {
+        return res.send(`<h2>Please go back and enter a movie title to search.</h2>`);
+    }
+
+    try{
+        const apiUrl = `https://${api_host}/api/imdb/search?originalTitle=${encodeURIComponent(searchMovie)}&rows=25&sortOrder=ASC&sortField=id`;
+        const apiRes = await fetch(apiUrl, {
+            method: "GET", 
+            headers: {
+                "Content-Type": "application/json",
+                "X-RapidAPI-Key" : api_key,
+                "X-RapidAPI-Host" : api_host
+            }
+        });
+
+        if (!apiRes.ok) {
+            const textError = await apiRes.text();
+            console.log(`RapidAPI Error Status Code: ${apiRes.status}`);
+            return res.status(apiRes.status).send(`<h2>API Error: Server returned status code ${apiRes.status}. Check your keys.</h2>`);
+        }
+    
+        const apiData = await apiRes.json();
+        console.log("Response: ", apiData);
+
+
+    }catch(err){
+        console.log("API ERROR: " + err);
+        res.status(500).send("Error reading data.");
+    }
+
+    res.send("Check your server terminal console to verify mixed movie/series results.");
+}
+)
 
 app.get("/favorites", (req,res) => {
     res.send(`<!DOCTYPE html>
@@ -77,7 +100,8 @@ app.get("/favorites", (req,res) => {
             </body>
         </html>
         `);
-})
+    }
+)
 app.listen(port, (err) => {
     if(err) {
         console.log("Server failed: " + err);
