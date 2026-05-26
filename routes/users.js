243f0d38dc2b2bb2ed7ebbfff21e58e5934807ce
No_Name <105019557+ah2355@users.noreply.github.com>
2026-05-26 14:55:27 -0400

    Fixed a bug where wrong file missed a quote mark

--- routes/users.js
+++ routes/users.js
@@ -99,7 +99,7 @@ router.post("/login", async (req,res) =>{
                     <head>
                         <meta charset = "utf-8">
                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <link rel = "stylesheet" href= "/routes/login.css>
+                        <link rel = "stylesheet" href= "/routes/login.css">
                         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
                         <link rel="icon" type="image/x-icon" href="/images/icon.png">
                         <title>SearchMovie</title>
